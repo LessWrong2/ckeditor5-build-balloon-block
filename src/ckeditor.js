9f523ab92324f468542b6e839dac5ee3e3b8ff99
Discordius <panisnecis@gmail.com>
2019-07-31 16:24:46 -0700

    Initial tutorial steps

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,91 +1,132 @@
 /**
  * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
+ * This file is licensed under the terms of the MIT License (see LICENSE.md).
  */
 
-// The editor creator to use.
-import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
+import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar';
+import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
+import ParagraphButtonUI from '@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui';
+import HeadingButtonsUI from '@ckeditor/ckeditor5-heading/src/headingbuttonsui';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import RealTimeCollaborativeEditing from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativeediting';
+import RealTimeCollaborativeComments from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativecomments';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import PresenceList from '@ckeditor/ckeditor5-real-time-collaboration/src/presencelist';
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 
-import '../theme/theme.css';
+export default class BalloonBlockEditorWithRealTimeCollaboration extends BalloonEditor { }
 
-export default class BalloonEditor extends BalloonEditorBase {}
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
-// Plugins to include in the build.
-BalloonEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
+// This SVG file import will be handled by webpack's raw-text loader.
+// This means that imageIcon will hold the source SVG.
+import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+
+class InsertImage extends Plugin {
+	init() {
+		const editor = this.editor;
+
+		editor.ui.componentFactory.add( 'insertImage', locale => {
+			const view = new ButtonView( locale );
+
+			view.set( {
+				label: 'Insert image',
+				icon: imageIcon,
+				tooltip: true
+			} );
+
+			// Callback executed once the image is clicked.
+			view.on( 'execute', () => {
+				const imageURL = prompt( 'Image URL' );
+			} );
+
+			return view;
+		} );
+	}
+}
+
+BalloonBlockEditorWithRealTimeCollaboration.builtinPlugins = [
 	Autoformat,
+	BlockQuote,
 	BlockToolbar,
 	Bold,
-	Italic,
-	BlockQuote,
-	CKFinder,
 	EasyImage,
+	Essentials,
 	Heading,
+	HeadingButtonsUI,
 	Image,
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
-	ImageUpload,
+	Italic,
 	Link,
 	List,
 	MediaEmbed,
+	MediaEmbedToolbar,
 	Paragraph,
+	ParagraphButtonUI,
 	PasteFromOffice,
+	PresenceList,
+	RealTimeCollaborativeComments,
+	RealTimeCollaborativeEditing,
+	RemoveFormat,
 	Table,
-	TableToolbar
+	TableToolbar,
+	InsertImage
 ];
 
-// Editor configuration.
-BalloonEditor.defaultConfig = {
-	blockToolbar: [
-		'heading',
+BalloonBlockEditorWithRealTimeCollaboration.defaultConfig = {
+	balloonToolbar: [
+		'bold',
+		'italic',
+		'removeFormat',
+		'link',
 		'|',
-		'bulletedList',
+		'comment',
+		'|',
+		'undo',
+		'redo',
+		'insertImage'
+	],
+	blockToolbar: [
+		'paragraph',
+		'heading1',
+		'heading2',
 		'numberedList',
-		'imageUpload',
+		'bulletedList',
 		'blockQuote',
+		'imageUpload',
 		'insertTable',
 		'mediaEmbed'
 	],
-	toolbar: {
-		items: [
-			'bold',
-			'italic',
-			'link',
-			'undo',
-			'redo'
-		]
-	},
 	image: {
 		toolbar: [
 			'imageStyle:full',
 			'imageStyle:side',
 			'|',
-			'imageTextAlternative'
+			'imageTextAlternative',
+			'|',
+			'comment'
 		]
 	},
 	table: {
@@ -93,8 +134,10 @@ BalloonEditor.defaultConfig = {
 			'tableColumn',
 			'tableRow',
 			'mergeTableCells'
-		]
+		],
+		tableToolbar: [ 'comment' ]
 	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	mediaEmbed: {
+		toolbar: [ 'comment' ]
+	}
 };
